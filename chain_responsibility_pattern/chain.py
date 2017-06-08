5b0f66b1feda5fe37ddc192b064d69d8c25d2b70
edward1chou <edwardchou903@icloud.com>
2017-06-08 21:53:30 +0800

    chain_responsibility pattern for one request and several object

---
+++ chain_responsibility_pattern/chain.py
@@ -0,0 +1,70 @@
+#! /bin/env python
+# -*- coding: utf-8 -*-
+"""
+module name: chain.py
+module description: implement of chain of responsibility pattern
+Authors: zhouchengyu(edwardchou903@icloud.com)
+Date:    2017/06/08 21:28
+"""
+
+
+class Event(object):
+    """
+    describe an event
+    """
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class Widget(object):
+    """
+    abstract class for widget
+    """
+    def __init__(self, parent=None):
+        self.parent = parent
+
+    def handle(self, event):
+        handler = 'handle_{}'.format(event)
+        if hasattr(self, handler):
+            method = getattr(self, handler)
+            method(event)
+        elif self.parent:
+            self.parent.handle(event)
+        elif hasattr(self, 'handle_default'):
+            self.handle_default(event)
+
+
+class MainWindow(Widget):
+    def handle_close(self, event):
+        print('MainWindow: {}'.format(event))
+
+    def handle_default(self, event):
+        print('MainWindow Default: {}'.format(event))
+
+
+class SendDialog(Widget):
+    def handle_paint(self, event):
+        print('SendDialog: {}'.format(event))
+
+
+class MsgText(Widget):
+    def handle_down(self, event):
+        print('MsgText: {}'.format(event))
+
+
+if __name__ == '__main__':
+    mw = MainWindow()
+    sd = SendDialog(mw)
+    msg = MsgText(sd)
+
+    for e in ('down', 'paint', 'unhandled', 'close'):
+        evt = Event(e)
+        print('\nSending event -{}- to MainWindow'.format(evt))
+        mw.handle(evt)
+        print('Sending event -{}- to SendDialog'.format(evt))
+        sd.handle(evt)
+        print('Sending event -{}- to MsgText'.format(evt))
+        msg.handle(evt)
